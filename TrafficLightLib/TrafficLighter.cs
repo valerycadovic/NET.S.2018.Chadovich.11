06d5b6432606e2ba6e3bc9f46cc11d016ba8ce9d
Valery Čadovič <valerachad03@gmail.com>
2018-07-28 01:16:12 +0300

    initial commit

---
+++ TrafficLightLib/TrafficLighter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrafficLightLib
+{
+    public class TrafficLighter
+    {
+        public IState State { get; set; }
+        
+        public TrafficLighter(IState state)
+        {
+            this.State = state ?? throw new ArgumentNullException($"{nameof(state)} is null");
+        }
+
+        public string Color { get; private set; }
+
+        public void Switch()
+        {
+            Color = State.Switch(this);
+        }
+    }
+}
